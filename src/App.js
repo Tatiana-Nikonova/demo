a6cf9aa3b83d8b3c94b27aa3dc51c3b27174e614
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-08-01 16:15:02 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from './Slider';
+
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<p>привет, я страница</p>
+			<Slider />
+		</>
+	)
+}
+
+export default App;
---
+++ src/Slider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import 'nouislider/dist/nouislider.css';
+
+function Slider() {	
+
+	return (
+		<div id="slider"></div>
+	)
+}
+export default Slider;
