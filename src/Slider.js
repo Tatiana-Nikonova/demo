bd0f5b4740af71aac507f2848deb3e892e3fb48d
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-08-02 10:58:54 +0300

    First

--- src/Slider.js
+++ src/Slider.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 
-function Slider() {	
+function Slider() {
+	var slider = document.getElementById('slider');
+	if (slider) {
+		return (
+			noUiSlider.create(slider, {
+				start: [20, 80],
+				connect: true,
+				range: {
+					'min': 0,
+					'max': 100
+				}
+			})
+		)
+	}
 
-	return (
-		<div id="slider"></div>
-	)
+
+
 }
 export default Slider;
